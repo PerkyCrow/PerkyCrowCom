eb2319186de1fb238cab40af0711c67f3922f0e8
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2023-02-17 23:44:27 +0100

    error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-
+  get '/404', to: 'errors#not_found'
+  get '/500', to: 'errors#internal_server'
+  get '/422', to: 'errors#unprocessable'
+  get '/406', to: 'errors#unacceptable'
   get '/legal_notice', to: 'pages#legal_notice'
 
   root "pages#home"
